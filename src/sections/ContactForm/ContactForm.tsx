7e0bfc1fb8dcbc0d0d3bfff0d350c9ff1f28332f
nimalchrist <selvanimal0@gmail.com>
2025-07-04 20:52:17 +0530

    fix: hotfix on main

--- src/sections/ContactForm/ContactForm.tsx
+++ src/sections/ContactForm/ContactForm.tsx
@@ -21,6 +21,12 @@ export default function ContactForm() {
     e.preventDefault();
     startLoading();
     try {
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
+      console.log(process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
       const safeData = JSON.parse(JSON.stringify(values));
       const contactService = new ContactService();
       await contactService.submitContactData(safeData);
